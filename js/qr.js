d8787e60d75821e8b07348453dc15dff2f4587cc
jnybergh <joacim@nybergh.se>
2013-05-27 17:32:27 +0200

    qr js

--- js/qr.js
+++ js/qr.js
@@ -1,5 +1,9 @@
 function qrReq() {
-	
+	$("#nMeals").keyup(function(event){
+		if(event.keycode == 13) {
+			console.log($("nMeals").html);
+		}
+	});
 	var meal1 = document.getElementById('meal1').value;
 	var meal2 = document.getElementById('meal2').value;
 	var meal3 = document.getElementById('meal3').value;
@@ -8,8 +12,10 @@ function qrReq() {
 	var meal6 = document.getElementById('meal6').value;
 
 	var rateUrl = "http://mlab1.msi.vxu.se/~jn222bd/qrproject/rate.php?dishes=";
+
 	var qrString = "<img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + rateUrl + meal1 +","+ meal2 +","+ meal3 +","+ meal4 +","+ meal5 +","+ meal6 + ">";
 
+	//alert(meal1 + meal2 + meal3 + meal4 + meal5 + meal6);
 	alert(qrString);
 
 	document.getElementById('QR').innerHTML=qrString;
