b6d3c1fa644f0734f2274b3528232c794fe66cea
jnybergh <joacim@nybergh.se>
2013-05-27 15:14:50 +0200

    added maps

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$("#group-wise").click(function() {
+	  alert("Handler for .click() called.");
+	});
+	$("#individually").click(function() {
+	  alert("Handler for .click() called.");
+	});
+});
