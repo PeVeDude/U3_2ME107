3736d46bd51f1834e825809035146b7c31f3d809
Fredrik <fidde83@gmail.com>
2013-05-27 15:50:01 +0200

    QR maker done?

--- js/qr.js
+++ js/qr.js
@@ -7,11 +7,12 @@ function qrReq() {
 	var meal5 = document.getElementById('meal5').value;
 	var meal6 = document.getElementById('meal6').value;
 
-	var rateUrl = "";
+	var rateUrl = "http://mlab1.msi.vxu.se/~jn222bd/qrproject/rate.html?dishes=";
 
-	var qrString = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + meal1 + meal2 + meal3 + meal4 + meal5 + meal6;
+	var qrString = "<img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + rateUrl + meal1 +","+ meal2 +","+ meal3 +","+ meal4 +","+ meal5 +","+ meal6 + ">";
 
 	//alert(meal1 + meal2 + meal3 + meal4 + meal5 + meal6);
 	alert(qrString);
 
+	document.getElementById('QR').innerHTML=qrString;
 }
