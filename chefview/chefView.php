2632b7cec5edc07618c7182a81776d3a9f4a5b1b
Fredrik <fidde83@gmail.com>
2013-05-27 16:25:36 +0200

    chefview

--- chefview/chefView.php
+++ chefview/chefView.php
@@ -8,21 +8,25 @@
     unset($_SESSION['event_id']);
 
 ?>
-<!DOC html>
+
+<html>
 <head>
 	<title>FoodReview</title>
-	<link href="style/main.css" type="text/css" rel="stylesheet" />
+	<link rel='stylesheet' href='../css/style.css' />
+    <script type='text/javascript' src='http://code.jquery.com/jquery-1.9.1.min.js'></script>
+    <script type='text/javascript' src='../js/qr.js'></script>
+    <link rel='stylesheet' href='../css/bootstrap.min.css'>
 </head>
 <body>
-	<div id="header">
+	<div id='header'>
            <h1>FoodReview</h1>
         </div>
-    <div id="login_form">
+    <div id='login_form'>
         <h2>Login</h2>
-            <form name="user_login" action="auth.php" method="post">
-                <p><label>Username: </label> <input type="text" name="username" size="30"/></p>
-                <p><label>Password: </label> <input type="password" name="pass" size="30"/></p>
-                <p><input type="submit" name="login" value="Log in" /></p>
+            <form name='user_login' action='auth.php' method='post'>
+                <p><label>Username: </label> <input type='text' name='username' size='30'/></p>
+                <p><label>Password: </label> <input type='password' name='pass' size='30'/></p>
+                <p><input type='submit' name='login' value='Log in' /></p>
             </form>
 	    <?php failed() ?>
     </div>
