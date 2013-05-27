29f1ca5030434e1598e862f72af970fdb87f2eb9
jnybergh <joacim@nybergh.se>
2013-05-27 16:24:22 +0200

    rate.php code

--- js/qr.js
+++ js/qr.js
@@ -7,7 +7,7 @@ function qrReq() {
 	var meal5 = document.getElementById('meal5').value;
 	var meal6 = document.getElementById('meal6').value;
 
-	var rateUrl = "http://mlab1.msi.vxu.se/~jn222bd/qrproject/rate.html?dishes=";
+	var rateUrl = "http://mlab1.msi.vxu.se/~jn222bd/qrproject/rate.php?dishes=";
 
 	var qrString = "<img src=https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + rateUrl + meal1 +","+ meal2 +","+ meal3 +","+ meal4 +","+ meal5 +","+ meal6 + ">";
 
