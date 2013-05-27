7bbd12ea8444c227ee790a66496098b9e4dd7678
Fredrik <fidde83@gmail.com>
2013-05-27 15:26:36 +0200

    qr stuff

---
+++ qr/qr.js
@@ -0,0 +1,14 @@
+function qrReq() {
+	
+	var meal1 = document.getElementById('meal1').value;
+	var meal2 = document.getElementById('meal2').value;
+	var meal3 = document.getElementById('meal3').value;
+	var meal4 = document.getElementById('meal4').value;
+	var meal5 = document.getElementById('meal5').value;
+	var meal6 = document.getElementById('meal6').value;
+
+	qrString = "";
+
+	alert(meal1 + meal2 + meal3 + meal4 + meal5 + meal6);
+
+}
