8966c1c2b7227814d7b3fdd305e462309608fddb
jnybergh <joacim@nybergh.se>
2013-05-29 15:31:16 +0200

    recommended dishes

--- js/index.js
+++ js/index.js
@@ -55,32 +55,4 @@ $(document).ready(function () {
 			}
 		});
 	});
-	/*$('#rate-ind input[type=checkbox]').each(function (i) {
-		i++;
-		console.log($('.checkbox'+ i +''));
-		$('#checkbox'+ i +'').change(function(){
-			if($('#checkbox'+ i +'').is(':checked')) {
-				$('.star'+ i +'').css("display", "block");
-			}
-			else if(!$('#checkbox'+ i +'').is('checked')) {
-				$('.star'+ i +'').css("display", "none");
-			}
-		});
-	});*/
- 	/*
- 	$( "input" ).on( "click", function() {
-	  $( "#rateDiv" ).html( $("input:checked").val() + " is checked!" );
-	});
-	*/
-	//$( "#rate-ind input" ).on( "change", countChecked );
-	/*
-	$("#rate-ind input[type=checkbox]").each(function(i) {
-		i++;
-        var $this = $(this);           
-        var student = $this.attr('data');
-                
-        var checkbox1 = $this.find('input.checkbox'+ i +'').is(':checked'); 
-        var checkbox2 = $this.find('input.checkbox'+ i +'').is(':checked'); 
-        //var checkbox3 = $this.find('input.checkbox'+ i +'').is(':checked'); 
-    });*/
 });
